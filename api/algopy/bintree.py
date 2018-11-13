c1566b290ef11036f3114ee99106207d2ff52124
Nathalie <nathalie.bouquet@epita.fr>
2018-11-13 08:23:35 +0100

    Add files via upload

---
+++ api/algopy/bintree.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+class BinTree: binary tree implementation
+Student version
+"""
+
+class BinTree:
+    def __init__(self, key, left, right):
+        """
+        Init Tree
+        """
+        self.key = key
+        self.left = left
+        self.right = right
+
